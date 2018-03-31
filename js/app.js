659fc576fd09ff0260987218a39609425c1c54f0
Adam Lewis <adamlewisp511@gmail.com>
2018-03-31 12:44:38 -0400

    added moveCounter

--- js/app.js
+++ js/app.js
@@ -63,6 +63,7 @@ var flipCard = function () {
 for (var i = 0; i < cards.length; i++) {
     card = cards[i];
     card.addEventListener("click", flipCard);
+    card.addEventListener("click", moveCounter);
 };
 /*Checks to see if the two cards match.
 If they match cards are locked in open position.
@@ -105,13 +106,9 @@ function restart() {
 
 const reset = document.querySelector(".restart");
 reset.addEventListener("click", restart);
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+function moveCounter() {
+    let movesCount = document.querySelector(".moves");
+    movesCount.innerHTML = move
+    //scorePanel();
+}
